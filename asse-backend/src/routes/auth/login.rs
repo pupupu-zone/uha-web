883ad25051ab38a89243638ddabeb9b9cb18f153
Aljóna Dzhukich <github@alena.red>
2024-11-03 13:44:16 +0500

    feat: add login endpoint

--- asse-backend/src/routes/auth/login.rs
+++ asse-backend/src/routes/auth/login.rs
@@ -1,15 +1,94 @@
-use serde_derive::Serialize;
+use actix_web::{web, Error, HttpResponse};
+use email_address::EmailAddress;
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+use sqlx::Row;
 
-#[derive(Serialize)]
-pub struct LoginResponse {
-    pub info: String,
+use crate::utils::acquire_pg_connection;
+
+use crate::errors::reg_errors;
+use crate::service::data_providers::WebDataPool;
+use crate::utils::auth::password::verify_password;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct LoginRequest {
+    pub email: String,
+    pub password: String,
 }
 
-#[tracing::instrument]
-pub async fn login() -> actix_web::HttpResponse {
-    tracing::event!(target: "backend", tracing::Level::DEBUG, "Login endpoint");
+pub async fn login(
+    user: web::Json<LoginRequest>,
+    dp: web::Data<WebDataPool>,
+    session: actix_session::Session,
+) -> Result<HttpResponse, Error> {
+    // Check if the email is valid
+    if EmailAddress::is_valid(&user.email) == false {
+        Err(reg_errors::email("Invalid E-Mail"))?
+    }
 
-    actix_web::HttpResponse::Ok().json(LoginResponse {
-        info: "Login endpoint".to_string(),
-    })
+    // Check if the password is valid
+    if user.password.is_empty() {
+        Err(reg_errors::password("Password too short"))?
+    }
+
+    // get user_id by email from db
+    let mut pg_connection = acquire_pg_connection(dp).await?;
+
+    match get_active_user(pg_connection, user.email.clone()).await {
+        Ok(logged_user) => {
+            // get hashed password for user_id from db
+            let is_pass_valid =
+                verify_password(logged_user.password.as_ref(), user.password.as_bytes()).await;
+
+            match is_pass_valid {
+                Ok(_) => {
+                    session.renew();
+
+                    session
+                        .insert(crate::types::USER_ID_KEY, logged_user.id)
+                        .expect("`user_id` cannot be inserted into session");
+
+                    session
+                        .insert(crate::types::USER_EMAIL_KEY, &logged_user.email)
+                        .expect("`user_email` cannot be inserted into session");
+
+                    Ok(HttpResponse::Ok().json(json!({
+                        "data": {
+                            "user_id": logged_user.id,
+                        }
+                    })))
+                }
+                Err(_e) => Err(reg_errors::not_authenticated("Invalid e-mail or password"))?,
+            }
+        }
+        Err(e) => Err(e),
+    }
+}
+
+pub async fn get_active_user(
+    mut pg_connection: sqlx::pool::PoolConnection<sqlx::Postgres>,
+    email: String,
+) -> Result<crate::types::auth::User, Error> {
+    match sqlx::query("SELECT id, email, password FROM users WHERE email = $1 AND is_active = TRUE")
+        .bind(email)
+        .map(|row: sqlx::postgres::PgRow| {
+            let selected_user = crate::types::auth::User {
+                id: row.get("id"),
+                email: row.get("email"),
+                password: row.get("password"),
+                is_active: true,
+            };
+
+            selected_user
+        })
+        .fetch_one(&mut *pg_connection)
+        .await
+    {
+        Ok(user) => Ok(user),
+        Err(e) => {
+            tracing::event!(target: "sqlx",tracing::Level::ERROR, "User not found in DB: {:#?}", e);
+
+            Err(reg_errors::not_authenticated("Invalid e-mail or password"))?
+        }
+    }
 }
--- asse-backend/src/routes/auth/logout.rs
+++ asse-backend/src/routes/auth/logout.rs
@@ -1,15 +1,21 @@
-use serde_derive::Serialize;
+use crate::errors::{info, reg_errors};
+use crate::utils::session_user_id;
 
-#[derive(Serialize)]
-pub struct LogoutResponse {
-    pub info: String,
-}
+use actix_web::{Error, HttpResponse};
 
-#[tracing::instrument]
-pub async fn logout() -> actix_web::HttpResponse {
-    tracing::event!(target: "backend", tracing::Level::DEBUG, "Logout endpoint");
+#[tracing::instrument(name = "Log out user", skip(session))]
+pub async fn logout(session: actix_session::Session) -> Result<HttpResponse, Error> {
+    match session_user_id(&session).await {
+        Ok(_) => {
+            tracing::event!(target: "backend", tracing::Level::INFO, "Users retrieved from the DB.");
+            session.purge();
 
-    actix_web::HttpResponse::Ok().json(LogoutResponse {
-        info: "Logout endpoint".to_string(),
-    })
+            Ok(info::system_msg("You have successfully logged out"))
+        }
+        Err(e) => {
+            tracing::event!(target: "backend", tracing::Level::ERROR, "Failed to get user from session: {:#?}", e);
+
+            Err(reg_errors::system("We can't log you out for now"))
+        }
+    }
 }
